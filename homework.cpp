46a744779e964e104dd9eff41d4f6d8c6c33d880
Edwinlinks <74891319+Edwinlinks@users.noreply.github.com>
2021-01-26 18:11:52 +0800

    Add files via upload

---
+++ homework.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+void funtion();
+int main()
+{
+	funtion();
+	return 0;
+}
+void funtion(){
+	
+	
+	int i=0,j=0,k=0;
+	int n,m;
+	for(k=0;k<10;k++)
+	{
+		m=10*k+5;
+		for(i=0;i<k;i++)
+		{
+			n=10*i+5;
+			cout<<n<<"*"<<m<<" ";
+		}
+		printf("\n");
+	}
+	
+	
+	
+	}
