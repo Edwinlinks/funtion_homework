835022025c0889cf515b07529f8d04f6ffacfdf6
Edwinlinks <74891319+Edwinlinks@users.noreply.github.com>
2021-01-26 18:07:51 +0800

    Add files via upload

---
+++ trans.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+using namespace std;
+void funtion();
+int main()
+{
+    
+    funtion();
+    return 0;
+}
+void funtion(){
+	
+	int a[9],num;
+
+    cout<<"input 0-255""<<endl;
+
+    while(cin>>num)
+    {
+        if(0<=num&&255>=num)
+
+        {
+
+            for(int i = 7; i >= 0; i--)
+
+            {
+                a[i]=(num>>i&1);
+            }
+
+            if(a[7]==0&&num<128)
+            {
+                cout<<"boy"<<endl;
+
+                cout<<"the student number:"<<num<<endl;
+
+                cout<<"the binary code is:";
+
+                for(int i = 7; i >=0; i--)
+
+                {
+                    cout<<a[i];
+                }
+
+                cout<<endl;
+            }
+
+            else
+
+            {
+                cout<<"girl"<<endl;
+
+                cout<<"the student number:"<<num-128<<endl;
+
+                cout<<"the binary code is:";
+
+                for(int i = 7; i >=0; i--)
+
+                {
+                    cout<<a[i];
+                }
+
+                cout<<endl;
+            }
+        }
+       else
+
+        {
+            cout<<"error"<<endl;
+        }
+
+      cout<<endl<<"input 0-255"<<endl;
+
+        }
+	
+	
+	
+	
+	
+	}
